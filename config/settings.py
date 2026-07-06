6fbebeeeefb25fa49cbb008d149434ab649fe89e
Tanya3701 <ip-bogomazova@mail.ru>
2026-07-06 15:19:27 +0300

    sh10

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
-SECRET_KEY =
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
@@ -54,8 +54,6 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
-
-
 
 if 'test' in sys.argv:
     DATABASES = {
